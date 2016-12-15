ab2a09bbd7abbca71c904489eb334fbbaabc47e2
askello <oleksii.korpan@gmail.com>
2016-12-15 10:29:10 +0200

    Updated lang helper

--- system/helper/lang.php
+++ system/helper/lang.php
@@ -1,6 +1,15 @@
 <?php
 if (!function_exists('lang')) {
 	function lang($key) {
-		return Registry::getInstance()->get('language')->get($key);
+		$text = Registry::getInstance()->get('language')->get($key);
+		
+		$arguments = func_get_args();
+		
+		if(count($arguments > 1)) {
+			array_shift($arguments);
+			return vsprintf($text, $arguments);
+		} else {
+			return $text;
+		}
 	}
 }
